866ab534539e42d1d95899af17e7780ec270ddbc
mmcclanahan <mmmcclan365@gmail.com>
2024-01-24 12:20:22 -0800

    complete functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //add a value to top of stack
+    //stack it
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function() {
+    //remove and return string on top of stack
+    //take off last number
+    var topIndex = Object.keys(storage).length - 1;
+    var deletedValue = storage[topIndex];
+    delete storage[topIndex];
+    return deletedValue;
   };
 
   someInstance.size = function() {
+    //return number of items in the stack
+    return Object.keys(storage).length;
   };
 
   return someInstance;
