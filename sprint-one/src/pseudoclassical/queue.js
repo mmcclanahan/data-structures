f15a0657bafd0658fb4141c668d64b751a62bf06
mmcclanahan <mmmcclan365@gmail.com>
2024-01-24 15:51:23 -0800

    complete pseudoclassical queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
-
+//because using new operator we can add to the Queue prototype object
+Queue.prototype.enqueue = function(string) {
+  //add to back of storage
+  this.storage[this.size()] = string;
+};
+Queue.prototype.dequeue = function() {
+  //remove first in storage return it and move all storage keys - 1
+  var removedValue = this.storage[0];
+  delete this.storage[0];
+  for (var key in this.storage) {
+    this.storage[key - 1] = this.storage[key];
+  }
+  delete this.storage[this.size() - 1];
+  return removedValue;
+};
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(string) {
+  //add string to back
+  this.storage[this.size()] = string;
+};
+Stack.prototype.pop = function() {
+  //remove and return the last value
+  var removedValue = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return removedValue;
+};
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
