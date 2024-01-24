b25354f8d06f58aa5abab26b71cf8d2b628ca118
mmcclanahan <mmmcclan365@gmail.com>
2024-01-24 12:16:21 -0800

    completed functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //add string back of que
+    var i = Object.keys(storage).length;
+    storage[i] = value;
   };
 
   someInstance.dequeue = function() {
+    //remove and return string at the front of queue
+    //get first
+    var deleted = storage[0];
+    delete storage[0];
+    var updatedStorage = {};
+    for (var key in storage) {
+      updatedStorage[key - 1] = storage[key];
+    }
+    storage = updatedStorage;
+    return deleted;
   };
 
   someInstance.size = function() {
+    //return number of items in queue
+    return Object.keys(storage).length;
   };
 
   return someInstance;
