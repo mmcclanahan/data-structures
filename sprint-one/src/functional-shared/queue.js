e937949a1be408a3280cc8e74c24cdcf46c5deb5
mmcclanahan <mmmcclan365@gmail.com>
2024-01-24 14:35:16 -0800

    completed functional-share queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
+//functional shared uses a container to hold all the methods for the instances
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    storage: {}
+  };
+  _.extend(instance, queueMethods);
+  return instance;
 };
 
 var queueMethods = {};
-
-
+queueMethods.enqueue = function(string) {
+  //add to back of queue
+  var i = Object.keys(this.storage).length;
+  this.storage[i] = string;
+};
+queueMethods.dequeue = function() {
+  //store front of queue
+  var deletedValue = this.storage[0];
+  //delete front of queue
+  delete this.storage[0];
+  //move queue up one
+  for (var key in this.storage) {
+    this.storage[key - 1] = this.storage[key];
+  }
+  delete this.storage[Object.keys(this.storage).length - 1];
+  //return the deletedvalue
+  return deletedValue;
+};
+queueMethods.size = function() {
+  //return size of the storage
+  return Object.keys(this.storage).length;
+};
