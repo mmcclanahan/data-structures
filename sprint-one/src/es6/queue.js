15b93a59fc78bb4a39ea76f6176d071404cd4b67
mmcclanahan <mmmcclan365@gmail.com>
2024-01-24 16:15:49 -0800

    completed es6 stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+  enqueue(string) {
+    //add to back
+    this.storage[this.size()] = string;
+  }
+  dequeue() {
+    //return front remove it and shift all keys - 1
+    var removedValue = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    delete this.storage[this.size() - 1];
+    return removedValue;
+  }
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,18 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
-
+  pop() {
+    //remove last
+    var removedValue = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return removedValue;
+  }
+  push(string) {
+    this.storage[this.size()] = string;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
